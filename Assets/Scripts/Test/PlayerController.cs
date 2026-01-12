3e61a9e755c8916fa9f56b49a7f23899c2653ea5
elliotbran <elliotfromlondon@gmail.com>
2026-01-12 18:53:06 +0100

    Movimiento temporal y camara cinemachine

---
+++ Assets/Scripts/Test/PlayerController.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.InputSystem;  
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] private float moveSpeed = 5f;
+
+    [SerializeField] private InputActionReference moveActionReference;
+
+    private Rigidbody2D rb;
+    private Vector2 movementInput;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();        
+    }
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movementInput.normalized * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    void OnMovePerformed(InputAction.CallbackContext ctx)
+    {
+        movementInput = ctx.ReadValue<Vector2>();
+    }
+
+    void OnMoveCanceled(InputAction.CallbackContext ctx)
+    {
+        movementInput = Vector2.zero;
+    }
+
+    private void OnEnable()
+    {
+        moveActionReference.action.Enable();
+        moveActionReference.action.performed += OnMovePerformed;
+        moveActionReference.action.canceled += OnMoveCanceled;
+    }
+
+    private void OnDisable()
+    {
+        moveActionReference.action.performed -= OnMovePerformed;
+        moveActionReference.action.canceled -= OnMoveCanceled;
+        moveActionReference.action.Disable();
+    }
+}
