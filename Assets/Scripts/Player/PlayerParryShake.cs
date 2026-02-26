2c607f1fe500bb6d9dd0fe562af5331e5250f4cf
RaulAlgasESP <algasraul@gmail.com>
2026-02-26 20:49:52 +0100

    factos

--- Assets/Scripts/Player/PlayerParryShake.cs
+++ Assets/Scripts/Player/PlayerParryShake.cs
@@ -4,7 +4,6 @@ public class PlayerParryShake : MonoBehaviour
 {
     public void TriggerShake()
     {
-        // Solo se activará cuando lo llamemos desde el PlayerController
         CinemachineShake.Instance.ShakeCamera(10f, .2f);
     }
 }
