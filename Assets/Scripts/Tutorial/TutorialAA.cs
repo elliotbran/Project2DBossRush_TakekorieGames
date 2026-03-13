9c2cfdefb0e0034704e096dc6c8f94f517675893
Komitb <iker.rubar27@gmail.com>
2026-03-13 01:03:35 +0100

    Bombo

---
+++ Assets/Scripts/Tutorial/TutorialAA.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using UnityEngine;
+
+public class TutorialAA : MonoBehaviour
+{
+    public GameObject izelStart;
+    private Animator animIzelStart;
+
+    public GameObject izelGetsHit;
+    private Animator animIzelHit;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        animIzelStart = izelStart.GetComponent<Animator>();
+        animIzelHit = izelGetsHit.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void startAnimAttack()
+    {
+        StartCoroutine(startAnimCoroutine());
+    }
+    public IEnumerator startAnimCoroutine()
+    {
+        animIzelStart.Play("Disappear");
+        yield return new WaitForSeconds(1f);
+        izelStart.SetActive(false);
+        yield return new WaitForSeconds(1f);
+        izelGetsHit.SetActive(true);
+    }
+}
